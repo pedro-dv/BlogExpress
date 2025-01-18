2bf5745c4159cab8850dda9759751b07431cca07
pedro-menezes <godofwar2906@gmail.com>
2025-01-18 15:28:28 -0300

    Mensagem explicando as alterações

--- database/database.js
+++ database/database.js
@@ -9,18 +9,20 @@ const connection = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     timezone: '-03:00',
     dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
   }
 );
 
 export default connection;
+
 
 
 // const connection = new Sequelize('DB-BlogExpress', 'root', 'L1nux2906*', {
