5d6bb6269b62f5f244e25de652a8aa0cd158fffd
pedro-menezes <godofwar2906@gmail.com>
2025-01-18 16:14:35 -0300

    Adicionando logs para verificar variáveis de ambiente

--- database/database.js
+++ database/database.js
@@ -1,24 +1,30 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+import mysql2 from "mysql2";
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 dotenv.config();
 
 const connection = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'mysql',
-    timezone: '-03:00',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  }
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: 'mysql',
+      dialectModule: mysql2,
+      timezone: '-03:00',
+      dialectOptions: isProduction
+        ? {
+            ssl: {
+              require: true,
+              rejectUnauthorized: false,
+            },
+          }
+        : {}, // Não usar SSL no desenvolvimento
+    }
 );
 
 export default connection;
